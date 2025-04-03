84314d4d6ccfcabff525bc9d3e3bec0786247cf0
sesilj <sesilj@gmail.com>
2025-04-03 14:16:13 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -7,7 +7,8 @@ public class pattern2 {
     public static void main(String args[]){
         for(int row =1 ; row<=5; row++){
                                
-            for(int column=1;column<=row;column++){                 
+            for(int column=1;column<=row;column++){  
+                //for(int column=1 ; column!=row; column++) {  
                             
                System.out.print("*");               
                 
