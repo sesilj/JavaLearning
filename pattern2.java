c033799d10d9d52e1e08de3ba706f9d799d536ac
sesilj <sesilj@gmail.com>
2025-04-03 14:35:48 +0530

    Update pattern2.java

--- pattern2.java
+++ pattern2.java
@@ -8,7 +8,7 @@ public class pattern2 {
         for(int row =1 ; row<=5; row++){
                                
             for(int column=1;column<=row;column++){  
-                //for(int column=1 ; column!=row; column++) {  
+                //for(int column=1 ; column!=row; column++) {  // this works but only partially since the first iteration comes out false in this loop
                             
                System.out.print("*");               
                 
