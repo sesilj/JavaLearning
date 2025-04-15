79cd7c7fcef6681870a6701507a8bd1faa33d953
sesilj <sesilj@gmail.com>
2025-04-15 18:59:58 +0530

    Update MainClass.java

--- MainClass.java
+++ MainClass.java
@@ -1,8 +1,11 @@
 public class MainClass {
 
     public static void main(String args[]){
+
+        // use of constructors 
        
         Student stu1 = new Student();
+
         Student stu2 = new Student(1111,55,"gop");
 
         Student stu3 = new Student(23);
@@ -11,17 +14,17 @@ public class MainClass {
 
         stu1.name="Gopan";
         stu1.age=33;
+        stu1.printInfo();
 
-        stu2.printInfo();
+        stu2.printInfo();  // without assigning any value to student object.
         
         stu2.name="VikaS";
         stu2.age=44;
         stu2.rollCall=2222;
+        stu2.printInfo();  // assigning value to stu2 
 
-        stu1.printInfo();
 
-        stu2.printInfo();  
-        stu3.printInfo();    
+        stu3.printInfo();    // assigning value only 
         
     }
     
@@ -49,17 +52,17 @@ public class MainClass {
     }
 
     Student(){  // constructor
-        this.rollCall =1234; 
+        this.rollCall =1234;  // this key word is used 
     }
 
-    Student(int roll, int age1, String name1){
+    Student(int roll, int age1, String name1){  // constructor with 3 variables
         this.rollCall = roll;
         this.age = age1;
         this.name = name1;       
 
     }
 
-    Student(int age2){
+    Student(int age2){  // constructor with 1 variable
 
         this.age=age2;
         if(age>40){
